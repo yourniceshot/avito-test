4cde668a8671ea79dd2a24e7a2d2ecb1406004b1
stacy mednikova <yourniceshot@gmail.com>
2020-01-07 14:02:21 +0300

    first final commit

--- src/js modules/popup.js
+++ src/js modules/popup.js
@@ -1,5 +1,7 @@
 import {pictureList} from './cards.js';
 const commentPopup = document.querySelector('.popup-mask');
+import {CommentList} from "./commentlist.js";
+import {commentContainer} from "./comment.js";
 
 export class Popup {
     constructor(popupElem, button) {
@@ -20,6 +22,7 @@ export class Popup {
     }
 }
 
+
 pictureList.addEventListener('click', function (event){
     event.stopPropagation();
     const { target } = event;
@@ -27,9 +30,53 @@ pictureList.addEventListener('click', function (event){
         const pictureButton = document.querySelector('.photo-grid__card');
         const popup = new Popup(commentPopup, pictureButton);
         popup.open();
+        commentContainer.innerHTML = '';
+
         const linkValue = target.style.backgroundImage;
         const popupImage = document.querySelector('.popup__photo');
         const linkValueFormatted = linkValue.slice(linkValue.indexOf('url') + 5, -2);
         popupImage.setAttribute('src', linkValueFormatted);
+
+        let commentList;
+        const imageId = linkValue.slice(30, -10);
+        fetch(`https://boiling-refuge-66454.herokuapp.com/images/${imageId}`)
+        .then(res => res.json())
+        .then((data)=> {
+            commentList = new CommentList(commentContainer, data);
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+
+        document.forms.form.addEventListener('submit', function(event) {
+            event.preventDefault();
+            const form = document.forms.form;
+            const comment = form.elements.comment;
+            const name = form.elements.name;
+            const getDate = new Date();
+            const year = getDate.getFullYear();
+            const month = getDate.getMonth();
+            const date = getDate.getDate();
+            const finalDate = `${date}.${month + 1}.${year}`;
+
+            fetch(`https://boiling-refuge-66454.herokuapp.com/images/${imageId}/comments`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                name: name.value,
+                comment: comment.value,
+                date: getDate
+            })
+            })
+            .then(()=> {
+                commentList.addComment(finalDate, comment.value);
+                form.reset();
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+        })
     }
-});
+})
